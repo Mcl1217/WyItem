3ba81fbd764f6d9409486356454c45151dd9337b
sange <278482496@qq.com>
2021-05-04 20:55:31 +0800

    'APP.vue新增git内容'

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 <script>
     // git 相关的指令
     /*
+    和上一次切换的分支进行分支转换 git checkout -
     查看日志相关的,git log/git log --oneline/git log --pretty=oneline/git reflog
     */
     import NavFooter from "./components/NavFooter/NavFooter";
