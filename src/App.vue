7aa9d126f04f1c298ef7d28d21a778f53385256a
sange <278482496@qq.com>
2021-05-04 20:48:02 +0800

    'APP.vue增加gitlog相关注释'

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,10 @@
 </template>
 
 <script>
+    // git 相关的指令
+    /*
+    查看日志相关的,git log/git log --oneline/git log --pretty=oneline/git reflog
+    */
     import NavFooter from "./components/NavFooter/NavFooter";
     export default {
         name: "App",
